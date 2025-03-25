bd8d829128177b4e355c87c401726873f732968b
kimsojin <100557009+rlathwls03@users.noreply.github.com>
2025-03-25 14:05:07 +0900

    add spring form functionality

--- src/main/java/kr/ac/hansung/cse/controller/controller/OfferController.java
+++ src/main/java/kr/ac/hansung/cse/controller/controller/OfferController.java
@@ -1,11 +1,16 @@
 package kr.ac.hansung.cse.controller.controller;
 
+import jakarta.validation.Valid;
 import kr.ac.hansung.cse.controller.model.Offer;
 import kr.ac.hansung.cse.controller.service.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -21,5 +26,32 @@ public class OfferController {
         model.addAttribute("id_offers", offers);
 
         return "offers";
+    }
+
+    @GetMapping("/createoffer")
+    public String createOffer(Model model) {
+        model.addAttribute("offer", new Offer()); // 빈 객체 입력
+        return "createoffer";
+    }
+
+    @PostMapping("docreate")
+    public String doCreate(Model model, @Valid Offer offer, BindingResult result) {
+        //System.out.println(offer);
+
+        if(result.hasErrors()) {
+            System.out.println("== Form data does not validate ==");
+
+            List<ObjectError> errors = result.getAllErrors();
+
+            for(ObjectError error:errors) {
+                System.out.println(error.getDefaultMessage());
+            }
+            return "createoffer"; // 사용자 입력 받은 객체 입력
+        }
+
+        // Controller -> Service -> Dao
+        // 에러가 없을 경우, 데이터베이스에 저장
+        offerService.insertOffer(offer);
+        return "offercreated";
     }
 }
